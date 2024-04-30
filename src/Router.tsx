228df59e36f2d9f04db622f36dc6229740478075
Thanakon <kimsanthanakon@gmail.com>
2024-04-30 18:46:45 +1000

    feat: add routing for playlist and converter pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import { VideoToGIF } from './pages/VideoToGIF.page';
+import { Playlist } from './pages/Playlist.page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: 'video-to-gif-converter',
+    element: <VideoToGIF />,
+  },
+  {
+    path: 'playlist-tool',
+    element: <Playlist />,
   },
 ]);
 
